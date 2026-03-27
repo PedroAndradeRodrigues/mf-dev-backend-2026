1bfc3dde96d28906dfc29c8c819fab27a52a2908
PedroAndradeRodrigues <pedro.andrade.rodrigues@gmail.com>
2026-03-27 14:51:17 -0300

    Criação e Configuração dos modelos de Veículo e Consumo

---
+++ mf-dev-backend-2023/Models/Consumo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+
+
+    }
+    public enum TipoCombustivel 
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+
+
+}
---
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+
+    }
+}
